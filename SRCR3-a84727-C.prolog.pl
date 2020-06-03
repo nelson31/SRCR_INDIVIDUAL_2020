08c66d09b024a02f0159cce69ca7656a3f0472dd
Nelson Faria <49026704+nelson31@users.noreply.github.com>
2020-06-03 09:47:05 +0100

    Atualizacao

--- SRCR3-a84727-C.prolog.pl
+++ SRCR3-a84727-C.prolog.pl
@@ -39,15 +39,275 @@
     	nao(excecao(adjacencia( Carreira,ParagemA,ParagemB,Dist ))).
 
 
+%-------------------------------------------------------------------------
+% ---------------IMPLEMENTACAO DAS FUNCIONALIDADES PRETENDIDAS------------
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Funcao que calcula a distancia euclidiana entre duas paragens dados a 
+% latitude e a longitude
+
+
+calculaDistEuc( LatA,LongA,LatB,LongB,Result ) :- 
+                        Result is sqrt( (LatB-LatA)^2 + (LongB-LongA)^2 ).
+
+%%%%%%%%%%%%%PESQUISA NAO INFORMADA%%%%%%%%%%%%%%%%%%%%%%%
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “primeiro em profundidade” sem custos para descobrir o caminho entre duas paragens
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+adjacente( Nodo,ProxNodo ) :- 
+                        demo(adjacencia( _,Nodo,ProxNodo,_ ),R), 
+                        (R == verdadeiro; !, fail).
+
+resolve_pp( Origem,Destino,[Origem|Caminho] ) :-
+                        profundidadeprimeiro( Origem,Destino,[],Caminho ), 
+                        getParagensbyIds( [Origem|Caminho],R ), 
+                        escrever( R ).
+
+profundidadeprimeiro( Destino,Destino,_,[] ).
+profundidadeprimeiro( Origem,Destino,Historico,[ProxNodo|Caminho] ) :-
+                        adjacente( Origem,ProxNodo ),
+                        nao(pertence(ProxNodo,Historico)),
+                        profundidadeprimeiro( ProxNodo,Destino,[ProxNodo|Historico],Caminho ).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “primeiro em profundidade” com custos para descobrir o caminho entre duas paragens
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+adjacente_c( Nodo,ProxNodo,Custo ) :- 
+                        demo(adjacencia( _,Nodo,ProxNodo,Custo ),R), 
+                        (R == verdadeiro; !, fail).
+
+resolve_pp_c( Origem,Destino,[Origem|Cam],Custo ) :-
+                        profundidadeprimeiro_c( Origem,Destino,[],Cam,C ),
+                        converteKms( C,Custo ),
+                        getParagensbyIds( [Origem|Cam],R ), 
+                        escrever( R ), nl,
+                        write("Total Custo do Caminho em Kms = "), 
+                        write(Custo).
+
+profundidadeprimeiro_c( Destino,Destino,Historico,[],0 ).
+profundidadeprimeiro_c( Origem,Destino,Historico,[ProxNodo|Caminho],Total ) :-
+                        adjacente_c( Origem,ProxNodo,C ),
+                        nao(pertence(ProxNodo,Historico)),
+                        profundidadeprimeiro_c( ProxNodo,Destino,[ProxNodo|Historico],Caminho,Custo ),
+                        Total is Custo+C.
+
+
+% Ver a melhor solução
+melhor_pp(Origem,Destino, S, Custo) :- findall((SS, CC), resolve_pp_c(Origem,Destino, SS, CC), L), 
+                        minimo(L, (S, Custo)).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “primeiro em largura” sem custos
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+resolve_pl( Origem,Destino,Caminho ) :-
+                        larguraprimeiro( Destino,[(Origem, [])|Xs]-Xs,[],Caminho ),
+                        getParagensbyIds( Caminho,R ), 
+                        escrever( R ).
+
+
+larguraprimeiro( Destino,[(Destino, Cams)|_]-_, _, Result ) :- 
+                        !, inverso( [Destino|Cams],Result ). % Devolve-se o inverso pk o caminho e construido ao contrario
+
+larguraprimeiro( Destino,[(Nodo, _)|Xs]-Ys,Historico,Result ) :- 
+                        pertence( Nodo,Historico ),!, % Se pertencer ao historico, significa que o nodo ja foi visitado, pelo que nao necessita de ser incluido nos nodos a visitar
+                        larguraprimeiro( Destino,Xs-Ys,Historico,Result ). % Continuamos ate encontrar a solucao
+
+larguraprimeiro( Destino,[(Nodo, Cams)|Xs]-Ys,Historico,Result ) :-
+                        adjacente(Nodo,ProxNodo),     % Usado somente para verificar se existem adjacentes
+                        findall( ProxNodo,adjacente(Nodo,ProxNodo),Lista ),   % Vamos buscar todos as paragens adjacentes
+                        atualizar( Nodo,Lista,Cams,[Nodo|Historico],Ys-Zs ),  % Atualizamos a Orla com os novos Nodos adjacentes a juntar
+                        larguraprimeiro( Destino,Xs-Zs,[Nodo|Historico],Result ). % Continuamos ate encontrar a solucao
+
+% Predicado Auxiliar usado para adicionar os novos nodos adjacentes encontrados à orla de maneira, 
+% fazendo-lhe corresponder o caminho que foi necessario efetuar ate chegar a esse nodo
+atualizar( _,[],_,_,X-X ).
+atualizar( Nodo,[ProxNodo|Resto],Cams,Historico,Xs-Ys ) :-
+                        pertence( ProxNodo,Historico ),!,
+                        atualizar( Nodo,Resto,Cams,Historico,Xs-Ys ).
+atualizar( Nodo,[ProxNodo|Resto],Cams,Historico,[(ProxNodo,[Nodo|Cams])|Xs]-Ys ):-
+                        atualizar( Nodo,Resto,Cams,Historico,Xs-Ys ).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “primeiro em largura” com custos 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+resolve_pl_c( Origem,Destino,Caminho,Custo ) :-
+                        larguraprimeiro_c( Destino,[(Origem, [], 0)|Xs]-Xs,[],Caminho,Custo1 ),
+                        converteKms( Custo1,Custo ),
+                        getParagensbyIds( Caminho,R ), 
+                        escrever( R ), nl,
+                        write("Total Custo do Caminho em Kms = "), 
+                        write(Custo), nl.
+
+
+larguraprimeiro_c( Destino,[(Destino, Cams, Custo)|_]-_, _, Result,Custo ) :- 
+                        !, inverso( [Destino|Cams],Result ). % Devolve-se o inverso pk o caminho e construido ao contrario
+
+larguraprimeiro_c( Destino,[(Nodo, _,_)|Xs]-Ys,Historico,Result ) :- 
+                        pertence( Nodo,Historico ),!, % Se pertencer ao historico, significa que o nodo ja foi visitado, pelo que nao necessita de ser incluido nos nodos a visitar
+                        larguraprimeiro_c( Destino,Xs-Ys,Historico,Result ). % Continuamos ate encontrar a solucao
+
+larguraprimeiro_c( Destino,[(Nodo, Cams, C)|Xs]-Ys,Historico,Result,Custo ) :-
+                        adjacente(Nodo,ProxNodo),     % Usado somente para verificar se existem adjacentes
+                        setof( (ProxNodo,C1),adjacente_c(Nodo,ProxNodo,C1),Lista ),   % Vamos buscar todos as paragens adjacentes
+                        atualizar_c( Nodo,C,Lista,Cams,[Nodo|Historico],Ys-Zs ),  % Atualizamos a Orla com os novos Nodos adjacentes a juntar
+                        larguraprimeiro_c( Destino,Xs-Zs,[Nodo|Historico],Result,Custo ). % Continuamos ate encontrar a solucao
+
+% Predicado Auxiliar usado para adicionar os novos nodos adjacentes encontrados à orla de maneira, 
+% fazendo-lhe corresponder o caminho que foi necessario efetuar ate chegar a esse nodo
+atualizar_c( _,_,[],_,_,X-X ).
+atualizar_c( Nodo,C,[ProxNodo|Resto],Cams,Historico,Xs-Ys ) :-
+                        pertence( ProxNodo,Historico ),!,
+                        atualizar_c( Nodo,C,Resto,Cams,Historico,Xs-Ys ).
+atualizar_c( Nodo,C,[(ProxNodo,C1)|Resto],Cams,Historico,[(ProxNodo,[Nodo|Cams],N)|Xs]-Ys ):-
+                        N is C+C1, atualizar_c( Nodo,C,Resto,Cams,Historico,Xs-Ys ).
+
+
+%%%%%%%%%%%%%PESQUISA INFORMADA%%%%%%%%%%%%%%%%%%%%%%%
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Predicados usados para estimativas
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+% Estmativa baseada na distancia Euclidiana entre uma origem e um destino
+estimabyDist( Origem,Destino,Estima ) :- 
+                        getLatitudebyId( Origem,LatO ), getLongitudebyId( Origem,LongO ),
+                        getLatitudebyId( Destino,LatD ),getLongitudebyId( Destino,LongD ),
+                        calculaDistEuc( LatO,LongO,LatD,LongD,Estima ).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “Pesquisa Gulosa (Greedy)” com custos
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+resolve_gulosa(Nodo, Caminho/Custo) :-
+    estima(Nodo, Estima),
+    gulosa([[Nodo]/0/Estima], InvCaminho/Custo/_),
+    inverso(InvCaminho, Caminho).
+
+gulosa(Caminhos, Caminho) :-
+    obtem_melhor_G(Caminhos, Caminho),
+    Caminho = [Nodo|_]/_/_,
+    goal(Nodo).
+
+gulosa(Caminhos, SolucaoCaminho) :-
+    obtem_melhor_G(Caminhos, MelhorCaminho),
+    seleciona(MelhorCaminho, Caminhos, OutrosCaminhos),
+    expande_gulosa(MelhorCaminho, ExpCaminhos),
+    append(OutrosCaminhos, ExpCaminhos, NovoCaminhos),
+    gulosa(NovoCaminhos, SolucaoCaminho).       
+
+
+obtem_melhor_G([Caminho], Caminho) :- !.
+
+obtem_melhor_G([Caminho1/Custo1/Est1,_/Custo2/Est2|Caminhos], MelhorCaminho) :-
+        Est1 =< Est2, !,
+        obtem_melhor_G([Caminho1/Custo1/Est1|Caminhos], MelhorCaminho).
+    
+obtem_melhor_G([_|Caminhos], MelhorCaminho) :- 
+    obtem_melhor_G(Caminhos, MelhorCaminho).
+
+expande_gulosa(Caminho, ExpCaminhos) :-
+    findall(NovoCaminho, adjacente(Caminho,NovoCaminho), ExpCaminhos).
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% >Uso da estratégia “Pesquisa A*” com custos
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+resolve_aestrela( Origem,Destino,Caminho,Custo ) :-
+                        estimabyDist( Origem,Destino,Estima ),
+                        aestrela( Destino,[[Origem]/0/Estima],Caminho1/Custo1/_ ),
+                        inverso( Caminho1,Caminho ),
+                        converteKms( Custo1,Custo ),
+                        getParagensbyIds( Caminho,R ), 
+                        escrever( R ), nl,
+                        write("Total Custo do Caminho em Kms = "), 
+                        write(Custo), nl.
+
+aestrela( Destino,Caminhos,Caminho ) :-
+                        obtem_melhor_caminho( Caminhos,Caminho ),
+                        Caminho = [Destino|_]/_/_.
+
+aestrela( Destino,Caminhos,SolucaoCaminho ) :-
+                        obtem_melhor_caminho( Caminhos,MelhorCaminho ),
+                        seleciona( MelhorCaminho,Caminhos,OutrosCaminhos ),
+                        expande_aestrela( Destino,MelhorCaminho,ExpCaminhos ),
+                        append( OutrosCaminhos,ExpCaminhos,NovoCaminhos ),
+                        aestrela( Destino,NovoCaminhos,SolucaoCaminho ).     
+
+
+obtem_melhor_caminho( [Caminho],Caminho ) :- !.
+
+obtem_melhor_caminho( [Caminho1/Custo1/Est1,_/Custo2/Est2|Caminhos],MelhorCaminho ) :-
+                        Custo1 + Est1 =< Custo2 + Est2, !,
+                        obtem_melhor_caminho( [Caminho1/Custo1/Est1|Caminhos],MelhorCaminho ).
+
+obtem_melhor_caminho( [_|Caminhos],MelhorCaminho ) :- 
+                        obtem_melhor_caminho( Caminhos,MelhorCaminho ).
+
+
+expande_aestrela( Destino,Caminho,ExpCaminhos ) :-
+                        findall( NovoCaminho,adjacente_aestrela( Destino,Caminho,NovoCaminho ),ExpCaminhos ).
+
+
+adjacente_aestrela( Destino,[Nodo|Caminho]/Custo/_,[ProxNodo,Nodo|Caminho]/NovoCusto/Est ) :-
+                        adjacente_c( Nodo,ProxNodo,Custo1 ),
+                        nao( pertence( ProxNodo,Caminho ) ),
+                        NovoCusto is Custo + Custo1,
+                        estimabyDist( ProxNodo,Destino,Est ).
+
+
+
+
+
+%-------------------------------------------------------------------------
+% ----------------PREDICADOS AUXILIARES UTEIS PARA AS FUNCIONALIDADES PRETENDIDAS-------------
+
+escrever([]).
+escrever([X|L]) :- write(X), nl,nl, escrever(L).
+
+getParagensbyIds( [],[] ).
+getParagensbyIds( [H|T],[paragem(H,A,B,C,D,E,F,G,I,J,K)|L] ) :- paragem(H,A,B,C,D,E,F,G,I,J,K), 
+                                                                getParagensbyIds(T,L).
+
+getLatitudebyId( Id,A ) :- demo( paragem(Id,A,B,C,D,E,F,G,I,J,K),R ), (R == verdadeiro; !, fail).
+
+getLongitudebyId( Id,B ) :- demo( paragem(Id,A,B,C,D,E,F,G,I,J,K),R ), (R == verdadeiro; !, fail).
+
+%-------------------------------------------------------------------------
+% ----------------ALGUMAS FUNCOES QUE PODEM SER UTEIS-------------
+
+%>--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Definição de um predicado que permite converter uma distancia de metros(m) 
+% para quilometros(Kms)
+
+converteKms( DistanciaM,DistanciaKm ) :- DistanciaKm is DistanciaM/1000.
+
+%>--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Definição de um predicado que permite converter uma distancia de metros(m) 
+% para quilometros(Kms)
+
+removeFirstElem( [],[] ).
+removeFirstElem( [H|Lista],Lista ).
+
+
 %>--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Definição de um predicado que permite calcular a soma de 
 % todos os elementos de uma lista de inteiros
 
 sum([],R) :-
-	R is 0.
+    R is 0.
 sum([H|T], R) :-
-	sum(T,RT),
-	R is H+RT.
+    sum(T,RT),
+    R is H+RT.
 
 %>--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Definição de um predicado que nos diz se os 
@@ -55,9 +315,8 @@ sum([H|T], R) :-
 equals([]).
 equals([H]).
 equals([F,S|T]) :-
-	F == S,
-	equals([S|T]).
-
+    F == S,
+    equals([S|T]).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -139,54 +398,30 @@ solucoes( X,Y,Z ) :-
 comprimento( S,N ) :-
     length( S,N ).
 
-
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
 pertence( X,[X|L] ).
 pertence( X,[Y|L] ) :-
     X \= Y,
     pertence( X,L ).
 
+inverso(Xs, Ys):-
+    inverso(Xs, [], Ys).
 
-%-------------------------------------------------------------------------
-% ---------------IMPLEMENTACAO DAS FUNCIONALIDADES PRETENDIDAS------------
+inverso([], Xs, Xs).
+inverso([X|Xs],Ys, Zs):-
+    inverso(Xs, [X|Ys], Zs).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Funcao que calcula a distancia euclidiana entre duas paragens dados a 
-% latitude e a longitude
-
-
-calculaDistEuc( LatA,LongA,LatB,LongB,Result ) :- 
-                            Result is sqrt( (LatB-LatA)^2 + (LongB-LongA)^2 ).
-
+seleciona(E, [E|Xs], Xs).
+seleciona(E, [X|Xs], [X|Ys]) :- seleciona(E, Xs, Ys).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
-caminho( A,B,P ) :- caminho1( A,[B],PP ), getFullInfo( PP,P ).
+minimo([(P,X)],(P,X)).
+minimo([(Px,X)|L],(Py,Y)) :- minimo(L,(Py,Y)), X>Y.
+minimo([(Px,X)|L],(Px,X)) :- minimo(L,(Py,Y)), X=<Y.
 
-caminho1( A,[A|P1],[A|P1] ).
-caminho1( A,[Y|P1],P ) :-
-        adjacencia( C,X,Y,D ),
-        \+ memberchk( X,[Y|P1] ),
-        caminho1( A,[X,Y|P1],P ).
-
-
-
-
-
-%-------------------------------------------------------------------------
-% Alguns predicados auxiliares uteis para o desenvolvimento das funcionalidades pretendidas: 
-
-escrever([]).
-escrever([X|L]) :- write(X), nl, escrever(L).
-
-getFullInfo( [],[] ).
-getFullInfo( [H|T],[paragem(H,A,B,C,D,E,F,G,I,J,K)|L] ) :- paragem(H,A,B,C,D,E,F,G,I,J,K), 
-                                                           getFullInfo(T,L).
-
-
-
-%-------------------------------------------------------------------------
-% Algumas funcoes Auxiliares que podem ser uteis: 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
 % Ordenada uma lista: 
 quickSort([],[]).
